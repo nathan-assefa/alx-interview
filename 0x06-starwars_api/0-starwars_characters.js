d14f6de2f5dbe63b3497b2862356ee09441d5f5d
nathan-assefa <aasamnew@gmail.com>
2023-09-07 19:48:48 +0000

    star wars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,22 +1,25 @@
 #!/usr/bin/node
 const request = require('request');
+const movieId = process.argv[2];
+const options = {
+  url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
+  method: 'GET'
+};
 
-function printCharacters (characters, index) {
-  if (index === characters.length) {
-    return;
-  }
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-    }
-    printCharacters(characters, index + 1);
-  });
-}
-
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
+request(options, function (error, response, body) {
   if (!error) {
     const characters = JSON.parse(body).characters;
     printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
